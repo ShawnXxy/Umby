da364632d39061a9e1bf12e23e86162e57af7ba0
ShawnXxy <shawnxxy@hotmail.com>
2019-01-06 23:06:47 +0800

    fix jobdispatcher compile bug

--- app/src/main/java/site/shawnxxy/umby/sync/FirebaseJobService.java
+++ app/src/main/java/site/shawnxxy/umby/sync/FirebaseJobService.java
@@ -1,29 +1,29 @@
 package site.shawnxxy.umby.sync;
 
-import android.app.job.JobParameters;
-import android.app.job.JobService;
 import android.content.Context;
 import android.os.AsyncTask;
+
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
 
 public class FirebaseJobService extends JobService {
 
     private AsyncTask<Void, Void, Void> fetchWeatherTask;
 
     @Override
-    public boolean onStartJob(final JobParameters params) {
-
+    public boolean onStartJob(final JobParameters job) {
         fetchWeatherTask = new AsyncTask<Void, Void, Void>() {
             @Override
             protected Void doInBackground(Void... voids) {
                 Context context = getApplicationContext();
                 SyncTask.syncWeather(context);
-                jobFinished(params, false);
+                jobFinished(job, false);
                 return null;
             }
 
             @Override
             protected void onPostExecute(Void avoid) {
-                jobFinished(params, false);
+                jobFinished(job, false);
             }
         };
 
@@ -32,8 +32,7 @@ public class FirebaseJobService extends JobService {
     }
 
     @Override
-    public boolean onStopJob(JobParameters params) {
-
+    public boolean onStopJob(JobParameters job) {
         if (fetchWeatherTask != null) {
             fetchWeatherTask.cancel(true);
         }
