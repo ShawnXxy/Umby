318340ec0eaf7858b93fa6241ff97e76dca5564d
ShawnXxy <shawnxxy@hotmail.com>
2019-01-06 20:50:47 +0800

    firebase job dispatcher

---
+++ app/src/main/java/site/shawnxxy/umby/sync/FirebaseJobService.java
@@ -0,0 +1,41 @@
+package site.shawnxxy.umby.sync;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Context;
+import android.os.AsyncTask;
+
+public class FirebaseJobService extends JobService {
+
+    private AsyncTask<Void, Void, Void> fetchWeatherTask;
+
+    @Override
+    public boolean onStartJob(final JobParameters params) {
+
+        fetchWeatherTask = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                Context context = getApplicationContext();
+                SyncTask.syncWeather(context);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void avoid) {
+                jobFinished(params, false);
+            }
+        };
+
+        fetchWeatherTask.execute();
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+
+        if (fetchWeatherTask != null) {
+            fetchWeatherTask.cancel(true);
+        }
+        return true;
+    }
+}
--- app/src/main/java/site/shawnxxy/umby/utilities/SyncUtils.java
+++ app/src/main/java/site/shawnxxy/umby/utilities/SyncUtils.java
@@ -8,12 +8,56 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.Driver;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.Trigger;
+
+import java.util.concurrent.TimeUnit;
+
+import site.shawnxxy.umby.sync.FirebaseJobService;
 import site.shawnxxy.umby.sync.SyncIntentService;
 import site.shawnxxy.umby.weatherData.WeatherContract;
 
 public class SyncUtils {
 
+    /**
+     *  Sync data every 3 hours
+     */
+    private static final int SYNC_INTERVAL_HOURS = 3;
+    private static final int SYNC_INTERVAL_SEC = (int) TimeUnit.HOURS.toSeconds(SYNC_INTERVAL_HOURS);
+    private static final int SYNC_FLEXTIME_SEC = SYNC_INTERVAL_SEC / 3;
+
     private static boolean initialized;
+
+    private static final String SYNC_TAG = "sync";
+
+    /**
+     *  Schedule periodic weather sync
+     */
+    static void scheduleWeatherSync(@NonNull final Context context) {
+        com.firebase.jobdispatcher.Driver driver = new GooglePlayDriver(context);
+        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(driver);
+
+        Job syncJob = dispatcher.newJobBuilder()
+                .setService(FirebaseJobService.class)
+                .setTag(SYNC_TAG)
+                .setConstraints(Constraint.ON_ANY_NETWORK)
+                .setLifetime(Lifetime.FOREVER) // set how long this sync job persists
+                .setRecurring(true)
+                .setTrigger(Trigger.executionWindow(
+                        SYNC_INTERVAL_SEC,
+                        SYNC_INTERVAL_HOURS + SYNC_FLEXTIME_SEC
+                ))
+                .setReplaceCurrent(true)
+                .build();
+
+        dispatcher.schedule(syncJob);
+
+    }
 
     synchronized public static void initialize(@NonNull final Context context) {
 
@@ -24,17 +68,17 @@ public class SyncUtils {
 
         initialized = true;
 
+        scheduleWeatherSync(context);
+
         /**
          *  Check if the weather content provider is empty.
          *  Performing a query on the main thread may cause UI slow loading.
          *  Use a new thread to
          */
-        new AsyncTask<Void, Void, Void>() {
+        Thread checkForEmpty = new Thread(new Runnable() {
             @Override
-            public Void doInBackground(Void... voids) {
-                // uri for each row of weather data from the weather table
+            public void run() {
                 Uri uri = WeatherContract.WeatherEntry.CONTENT_URI;
-
                 String[] projectionColumns = {WeatherContract.WeatherEntry._ID};
                 String selectionStmt = WeatherContract.WeatherEntry.getSqlSelectForToday();
 
@@ -51,9 +95,34 @@ public class SyncUtils {
 
                 // avoid memory leaks
                 cursor.close();
-                return null;
             }
-        }.execute();
+        });
+//        new AsyncTask<Void, Void, Void>() {
+//            @Override
+//            public Void doInBackground(Void... voids) {
+//                // uri for each row of weather data from the weather table
+//                Uri uri = WeatherContract.WeatherEntry.CONTENT_URI;
+//
+//                String[] projectionColumns = {WeatherContract.WeatherEntry._ID};
+//                String selectionStmt = WeatherContract.WeatherEntry.getSqlSelectForToday();
+//
+//                Cursor cursor = context.getContentResolver().query(uri,
+//                        projectionColumns,
+//                        selectionStmt,
+//                        null,
+//                        null);
+//
+//                // Sync weather data if cursor is null
+//                if (null == cursor || cursor.getCount() == 0) {
+//                    startSyncImmediately(context);
+//                }
+//
+//                // avoid memory leaks
+//                cursor.close();
+//                return null;
+//            }
+//        }.execute();
+
     }
 
     public static void startSyncImmediately (@NonNull final Context context) {
