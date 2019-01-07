fa5fa766e94319c3deab04d49b1044904bc4a19d
ShawnXxy <shawnxxy@hotmail.com>
2019-01-07 22:36:27 +0800

    bind data with views

--- app/src/main/java/site/shawnxxy/umby/WeatherAdapter.java
+++ app/src/main/java/site/shawnxxy/umby/WeatherAdapter.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import site.shawnxxy.umby.utilities.DayUtils;
@@ -50,7 +51,7 @@ class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherAdapterV
         boolean attachToParent = false;
 
         View view = inflater.inflate(weatherDataList, viewGroup, attachToParent);
-        view.setFocusable(true);
+//        view.setFocusable(true);
         return new WeatherAdapterViewHolder(view);
     }
 
@@ -64,22 +65,38 @@ class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherAdapterV
 
         cursor.moveToPosition(position);
 
-        // read data from the cursor
+        //  Match weather icon
+        int weatherId = cursor.getInt(MainActivity.INDEX_WEATHER_CONDITION_ID);
+        int weatherImageId;
+        weatherImageId = WeatherUtils.getSmallWeatherIcon(weatherId);
+        weatherAdapterViewHolder.iconImageView.setImageResource(weatherImageId);
+
+        // read data from the cursor based on date
         long dateInMillis = cursor.getLong(MainActivity.INDEX_WEATHER_DATE);
         String dateStr = DayUtils.dateFormatted(context, dateInMillis, false);
+        weatherAdapterViewHolder.dateTextView.setText(dateStr);
 
         // Use weather id to get weather description
-        int weatherId = cursor.getInt(MainActivity.INDEX_WEATHER_CONDITION_ID);
-        String description = WeatherUtils.getWeatherCondition(context, weatherId);
+        String descriptionStr = WeatherUtils.getWeatherCondition(context, weatherId);
+        String descriptionAlly = context.getString(R.string.a11y_forecast, descriptionStr);
+        weatherAdapterViewHolder.descriptionTextView.setText(descriptionStr);
+        weatherAdapterViewHolder.descriptionTextView.setContentDescription(descriptionAlly);
 
         // get high and low temperature
         double highInCels = cursor.getDouble(MainActivity.INDEX_WEATHER_MAX_TEMPERATURE);
         double lowInCels = cursor.getDouble(MainActivity.INDEX_WEATHER_MIN_TEMPERATURE);
-        String highAndLow = WeatherUtils.formatHighLow(context, highInCels, lowInCels);
+        String highStr = WeatherUtils.formatTemperature(context, highInCels);
+        String lowStr = WeatherUtils.formatTemperature(context, lowInCels);
+        String highAlly = context.getString(R.string.a11y_high_temp, highStr);
+        String lowAlly = context.getString(R.string.a11y_low_temp, lowStr);
+        weatherAdapterViewHolder.highTemperatureTextView.setText(highStr);
+        weatherAdapterViewHolder.highTemperatureTextView.setContentDescription(highAlly);
+        weatherAdapterViewHolder.lowTemperatureTextView.setText(lowStr);
+        weatherAdapterViewHolder.lowTemperatureTextView.setContentDescription(lowAlly);
 
-        String weatherSummary = dateStr + " - " + description + " - " + highAndLow;
-
-        weatherAdapterViewHolder.weatherDataTextView.setText(weatherSummary);
+//        String highAndLow = WeatherUtils.formatHighLow(context, highInCels, lowInCels);
+//        String weatherSummary = dateStr + " - " + description + " - " + highAndLow;
+//        weatherAdapterViewHolder.weatherDataTextView.setText(weatherSummary);
 
     }
 
@@ -88,11 +105,22 @@ class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherAdapterV
      */
     class WeatherAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        final TextView weatherDataTextView;
+//        final TextView weatherDataTextView;
+        final TextView dateTextView;
+        final TextView descriptionTextView;
+        final TextView highTemperatureTextView;
+        final TextView lowTemperatureTextView;
+        final ImageView iconImageView;
 
         WeatherAdapterViewHolder(View view) {
             super(view);
-            weatherDataTextView = view.findViewById(R.id.weather_data_textview);
+//            weatherDataTextView = view.findViewById(R.id.weather_data_textview);
+            dateTextView = view.findViewById(R.id.date);
+            descriptionTextView = view.findViewById(R.id.weather_description);
+            highTemperatureTextView = view.findViewById(R.id.high_temperature);
+            lowTemperatureTextView = view.findViewById(R.id.low_temperature);
+            iconImageView = view.findViewById(R.id.weather_icon);
+
             // Implement click listener
             view.setOnClickListener(this);
         }
